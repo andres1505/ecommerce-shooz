febe0ff7538889117646e86aabd4b979b3f99264
EmanuelMolano <molanoemanuel@gmail.com>
2023-05-31 11:55:46 -0500

    add: Cambios catalogo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Cuerpo from "./assets/componentes/cuerpo/cuerpo"
+import './App.css'
+import Catalogo from "./assets/componentes/catalogo/catalogo"
+
+
 function App() {
   
-
   return (
     <>
     <Cuerpo></Cuerpo>
+    <Catalogo />
     </>
   )
 }
