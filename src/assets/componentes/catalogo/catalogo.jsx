a9f7d97812fc0c4cdf681258f9b67c9ce4b8ef42
EmanuelMolano <molanoemanuel@gmail.com>
2023-06-01 16:28:52 -0500

    <modHeader>

--- src/assets/componentes/catalogo/catalogo.jsx
+++ src/assets/componentes/catalogo/catalogo.jsx
@@ -1,34 +1,31 @@
 import React from "react";
 import style from "./catalogo.module.css";
-import naranja from "./imgs/naranja.jpg";
-import red from "./imgs/redb.jpg";
-import tres from "./imgs/tres.jpg";
-import rosa from "./imgs/rosa.jpg";
-import baw from "./imgs/baw.jpg";
+import naranja from "./imgs/naranja.webp";
+import red from "./imgs/redb.webp";
+import tres from "./imgs/tres.webp";
+import rosa from "./imgs/rosa.webp";
 import FuncionW from "./funcion/funcionW";
 import FuncionM from "./funcion/funcionM";
-
 
 const Catalogo = () => {
   return (
     <>
       <div className={style.for}>
-        <h2>FOR HER</h2>
+        <h2 className={style.h2}>FOR HER</h2 >
       </div>
-    <FuncionW />
-    <div className={style.gallery}>
-          <img src={naranja} alt="nike" />
-          <img src={red} alt="nike" />
-          <img src={tres} alt="nike" />
-          <img src={rosa} alt="nike" />
-        </div>
-        <div className={style.for}>
-        <h2>FOR HIM</h2>
+      <FuncionW />
+      <div className={style.gallery}>
+        <img src={naranja} alt="nike" />
+        <img src={red} alt="nike" />
+        <img src={tres} alt="nike" />
+        <img src={rosa} alt="nike" />
+      </div>
+      <div className={style.for}>
+        <h2 className={style.h2}>FOR HIM</h2 >
       </div>
       <FuncionM />
     </>
-  )
-}
+  );
+};
 
 export default Catalogo;
-
