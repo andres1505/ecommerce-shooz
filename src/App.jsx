1372dc20f241f564d4e73598b975c9a367a90e09
vicmana180 <vmnaviam@gmail.com>
2023-06-01 19:32:50 -0500

    pie de pagina

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Cuerpo from "./assets/componentes/cuerpo/cuerpo"
 import './App.css'
 import Catalogo from "./assets/componentes/catalogo/catalogo"
 import Header from "./assets/componentes/header/header"
+import Footer from "./assets/componentes/Footer/footer"
 
 function App() {
   
@@ -9,6 +10,7 @@ function App() {
     <>
     <Header />
     <Catalogo />
+    <Footer/>
     </>
   )
 }
---
+++ src/assets/componentes/Footer/footer.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+
+
+
+
+const Footer =()=>{
+
+
+    return(
+
+        <div class="container">
+        <footer class="py-3 my-4">
+          <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Inicio</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Caracteristicas</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Acerca</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Historia</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Soporte</a></li>
+          </ul>
+          <p class="text-center text-body-secondary">&copy; 2023 Shooz, Inc</p>
+        </footer>
+      </div>
+    )
+}
+
+
+export default Footer
