cecc79ce7f7ac4219343b34f6984e3f6966c6827
andres1505 <andreslunatorrejano@gmail.com>
2023-06-02 13:03:40 -0500

    homeV1

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Catalogo from "./assets/componentes/catalogo/catalogo"
 import Header from "./assets/componentes/header/header"
 import Footer from "./assets/componentes/Footer/footer"
+import Home from './assets/componentes/home/Home'
 import React from 'react';
 
 
