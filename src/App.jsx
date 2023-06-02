a74a0ed5df041fdca997c7041f418c39e306aa73
Hvasquez9 <hectorvasquezcarrilllo@gmail.com>
2023-06-01 20:44:33 -0500

    carrito

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Catalogo from "./assets/componentes/catalogo/catalogo"
 import Header from "./assets/componentes/header/header"
 import Footer from "./assets/componentes/Footer/footer"
+import Carro from "./assets/componentes/carro/carro"
 
 function App() {
   
@@ -11,6 +12,7 @@ function App() {
     <Header />
     <Catalogo />
     <Footer/>
+    <Carro></Carro>
     </>
   )
 }
